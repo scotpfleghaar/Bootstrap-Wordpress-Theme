67f84f5354922a1756605174c3fa51ea06e5d907
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-11-22 10:02:03 -0600

    inital Commit
    Wordpress installed, and the template is started

---
+++ wordpress/wp-content/themes/bootstrapwptheme/footer.php
@@ -0,0 +1,13 @@
+
+<div class="container">
+    <hr>
+    <footer>
+        <p>&copy; 2017 - BootPress</p>
+    
+    </footer>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/bootstrap/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wordpress/wp-content/themes/bootstrapwptheme/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BootPress</title>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
+</head>
+<body>
+    <nav class="navbar navbar-default">
+    <div class="container">
+       
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php bloginfo('url');?>"><?php bloginfo('name'); ?></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        
+        <form class="navbar-form navbar-left">
+           
+        </form>
+       
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+    </nav>
